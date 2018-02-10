0b12f2bea3e26436ade68da9ffb0895c08e65780
Angel <angeljpp29@gmail.com>
2018-02-10 16:04:36 -0600

    Vista de Lista y Creación de personas

---
+++ Lab1_ED/Lab1_ED/Controllers/PersonaController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab1_ED.DBContext;
+
+namespace Lab1_ED.Controllers
+{
+    public class PersonaController : Controller
+    {
+        DefaultConnection db = DefaultConnection.getInstance;
+
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View(db.Personas.ObtenerListado());
+        }
+
+        // GET: Persona/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Persona/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Persona/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="PersonaID, Nombre, Apellido, Edad")]Models.Persona persona)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                db.Personas.Insertar(persona);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Persona/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Persona/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Persona/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Persona/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab1_ED/Lab1_ED/DBContext/DefaultConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TDA;
+
+namespace Lab1_ED.DBContext
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+        public ListaOriginal<Models.Persona> Personas = new ListaOriginal<Models.Persona>();
+        public ListaOriginal<string> Ids = new ListaOriginal<string>();
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+
+                return Instance;
+            }
+        }
+
+    }
+}
